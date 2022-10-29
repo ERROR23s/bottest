c8cfffeea7f38d5c47b1953b71bf7d8fb57af641
Pavel <Shonowin@gmail.com>
2022-10-29 21:50:01 +0300

    create timer command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
+import time
 
 from wiki import serchWiki
 
@@ -16,7 +17,13 @@ def wikiSelecter(update, context):
         update.message.reply_text("Вы не ввели данные")
 
 def start(update, context):
-    update.message.reply_text("Начинаю свою работу")
+    update.message.reply_text("Начинаю свою работу. Для большей информации напишите /help")
+
+def help(update, context):
+    update.message.reply_text("Бот поддерживет следующие команды:"
+                              "\n/timer - запуск таймера"
+                              "\n/wiki - запрос к википедии"
+                              "\n/help - вывод команд")
 
 def echo(update, context):
     txt = update.message.text
@@ -24,13 +31,25 @@ def echo(update, context):
         txt = 'Ну привет'
     update.message.reply_text(txt)
 
+def timer(update, context):
+    sec = "".join(context.args)
+    sec = int(sec)
+    update.message.reply_text(f"Таймер запущен {int(sec/60)}:{(sec%60):02} до конца")
+    while sec > 0:
+        time.sleep(1)
+        sec -= 1
+    update.message.reply_text("Таймер закончил свою работу")
+
+
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     print("Бот запущен...")
 
     dp.add_handler(CommandHandler("wiki", wikiSelecter))
+    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("timer", timer))
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
