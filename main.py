c045476466e610e092d0d8057981f39559c087e1
201-06 <student@201-06>
2022-10-29 10:37:03 +0300

    wiki

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
 
+from wiki import serchWiki
+
 TOKEN = "5683994914:AAHvBl6EUt-qvS27aJANTefwe5LBVB6kAdU"
+
+def wikiSelecter(update, context):
+    print(context.arg)
+    word = "".join(context.arg)
+    if word:
+        update.message.reply_text("Идет поиск...")
+        summary, url =serchWiki(word)
+        update.message.reply_text(summary + url)
+    else:
+        update.message.reply_text("Вы не ввели данные")
 
 def start(update, context):
     update.message.reply_text("Начинаю свою работу")
@@ -17,6 +29,7 @@ def main():
     dp = updater.dispatcher
     print("Бот запущен...")
 
+    dp.add_handler(CommandHandler("wiki", wikiSelecter))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text, echo))
 
---
+++ wiki.py
@@ -0,0 +1,13 @@
+import wikipedia
+wikipedia.set_lang("ru")
+
+def serchWiki(word):
+    try:
+        w = wikipedia.search(word)
+        if w:
+            w2 = wikipedia.page(word).url
+            w1 = wikipedia.summary(word)
+            return w1,f"\n Ссылка: {w2}"
+        return "Запрос в википедии не найден" , ""
+    except:
+        return "Википедия недоступна. Повторите позже." , ""
